0e85a57a74b11ac452dcd4ff56a89b9f74ae7d1b
Tony Fast <tony.fast@gmail.com>
2016-04-17 16:09:13 -0400

    Update literacy.py

--- literacy.py
+++ literacy.py
@@ -10,43 +10,45 @@ class LiterateEnvironment( jinja2.Environment ):
         return """"""
     def __init__(self,default_lang='',lang_prefix='lang-', filter_prefix='execute_'):
         super().__init__(loader = jinja2.DictLoader({}))
-        self.globals = {**self.globals,"default_lang": default_lang, "lang_prefix": lang_prefix, "filter_prefix": filter_prefix}
+        self.globals = {
+            **self.globals,
+            "default_lang": default_lang, "lang_prefix": lang_prefix, "filter_prefix": filter_prefix,'render_data':True,'name_cells':True}
         self.filters[self.globals['filter_prefix']+'python'] = self._execute_python
         self.filters[self.globals['filter_prefix']+'javascript'] = lambda s: (s, None)
-        
+
 class Compiler:
     def execute(self, child, data={}, filter_name=""""""):
         lang = self.env.globals['default_lang']
         if child('code').attr('class'):
             lang = [c.lstrip(self.env.globals['lang_prefix']) for c in child('code').attr('class').split() if c.startswith(self.env.globals['lang_prefix'])][0]
         filter_name = self.env.globals['filter_prefix']+lang
-        rendered = self.render(self._append_template(child.outerHtml()),data)
+        rendered = self.render(child.outerHtml(),data,render_data=True)
         if filter_name in self.env.filters:
             src = self.env.filters[filter_name](PyQuery(rendered).text())
             rendered += """<script>%s</script>"""%src if src else """"""
         return rendered
-    
+
 class Templates(Compiler):
     templates=[]
     @property
     def _current_template(self):
         return self.env.globals[self.env.globals['_current_name']]
-    
-    def _append_template(self, txt, data={}):
+
+    def render(self,txt, data={},render_data=False):
         template = self.env.from_string(txt)
         if '_current_name' in self.env.globals:
+            # Save the templates if teh cell is named
             self._current_template.templates.append(template)
-        return self.render(template, data)
-    
-    def render(self,template, data={}):
-        return self._current_template.templates[-1].render({
-            **{k:getattr(self.env.ip.user_ns['__builtin__'],k) for k in dir(self.env.ip.user_ns['__builtin__']) if not k.startswith('_')},
-            **self.env.ip.user_ns, **data,
-        })
+        if self.env.globals['render_data'] or render_data:
+            return template.render({
+                **{k:getattr(self.env.ip.user_ns['__builtin__'],k) for k in dir(self.env.ip.user_ns['__builtin__']) if not k.startswith('_')},
+                **self.env.ip.user_ns, **data,
+            })
+        return txt
 
 class Selection(IPython.display.HTML, Templates):
     def __init__(self,data):
-        super().__init__( data ) 
+        super().__init__( data )
         self.raw, self.query = [self.data, PyQuery(self.renderer(self.data))]
         self.data = self.parse(self.query)
     def parse( self, query=None, data={} ):
@@ -57,21 +59,21 @@ class Selection(IPython.display.HTML, Templates):
             if not is_code:
                 block += child.outerHtml()
             if is_code and block:
-                html += '\n' + self._append_template(block, data)
+                html += '\n' + self.render(block, data)
                 block = """"""
             if is_code:
                 block += """<hr>%s<hr>"""%self.execute(child,data)
-        if block:
-            html += '\n' + self._append_template(block, data)
+        else:
+            html += '\n' + self.render(block, data) if block else """"""
         return html
-    
-class Cell( Selection ):   
+
+class Cell( Selection ):
     renderer = mistune.Markdown( renderer=mistune.Renderer(escape=False))
     def __init__(self, data, name=haikunator.haikunate(), env=LiterateEnvironment()):
         self.env = env
         self.name = name
         self.env.globals[self.name] = self
-        super().__init__(data)        
+        super().__init__(data)
 
 @IPython.core.magic.magics_class
 class Literate(IPython.core.magic.Magics):
@@ -84,7 +86,7 @@ class Literate(IPython.core.magic.Magics):
         super().__init__()
         self.env.ip.register_magics(self)
 
-    @IPython.core.magic.cell_magic 
+    @IPython.core.magic.cell_magic
     @IPython.core.magic_arguments.magic_arguments()
     @IPython.core.magic_arguments.argument( "name", default=None, nargs="?", help="""Name of local variable to set to parsed value""")
     @IPython.core.magic_arguments.argument("-n", "--nodisplay", default=False, action="store_true", help="""Show the output""")
@@ -92,12 +94,12 @@ class Literate(IPython.core.magic.Magics):
         line = line.strip()
         args = IPython.core.magic_arguments.parse_argstring(self.literate, line)
         define_global = True
-        if not args.name:
+        if args.name:
+            self.env.globals['_current_name'] = args.name
+        else:
             define_global = False
             args.name = haikunator.haikunate(delimiter='_',tokenlength=0)
-        self.env.globals['_current_name'] = args.name
         display = Cell( cell, args.name, self.env )
-        self.cells[args.name] = display
         if define_global:
             self.env.ip.user_ns[args.name] = display
         if not args.nodisplay:
