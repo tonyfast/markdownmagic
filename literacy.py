4ff5ca62ac77d199177c06e96cab2d9a49d3c0f7
Tony Fast <tony.fast@gmail.com>
2016-04-22 21:22:00 -0400

    Update literacy.py

--- literacy.py
+++ literacy.py
@@ -1,106 +1,138 @@
-import jinja2, IPython, mistune, haikunator
+import jinja2, IPython, mistune
 from pyquery import PyQuery
+import yaml
 get_ipython = IPython.get_ipython
 
 class LiterateEnvironment( jinja2.Environment ):
     """A Jinja Environment that can """
     ip = get_ipython()
-    def _execute_python( self, code ):
-        self.ip.run_cell(code)
-        return """"""
-    def __init__(self,default_lang='',lang_prefix='lang-', filter_prefix='execute_',render_data=True):
+    def __init__(self,default_lang='python',lang_prefix='lang-', render_data=True):
         super().__init__(loader = jinja2.DictLoader({}))
         self.globals = {
             **self.globals,
-            "default_lang": default_lang, "lang_prefix": lang_prefix, "filter_prefix": filter_prefix,'render_data':render_data,'name_cells':True}
-        self.filters[self.globals['filter_prefix']+'python'] = self._execute_python
-        self.filters[self.globals['filter_prefix']+'javascript'] = lambda s: (s, None)
-
-class Compiler:
-    def execute(self, child, data={}, filter_name=""""""):
+            "default_lang": default_lang, "lang_prefix": lang_prefix, 'render_data':render_data
+        }
+        self.globals['callback']={
+            'python': lambda code: {
+                'python': self.ip.run_cell(code),
+            },
+            'html': lambda code: {'html': code}, 'js': lambda code: {'js': code}, 'css': lambda code: {'css': code},
+        }
+class Weave():
+    def __init__(self):
+        if not 'weave_template' in self.env.loader.mapping:
+            self.env.loader.mapping['weave_template'] = """{% if css %}<style>{{css}}</style>{% endif %}{% if html %}{{html}}<hr>{% endif %}{% if js %}<script>{{js}}</script>{% endif %}"""
+    def weave(self,**kwargs):
+        return self.env.get_template('weave_template').render(
+            {**{'html':'','js':'','css':''},**kwargs}
+        )
+class TangleClient(Weave):
+    def tangle(self,code):
+        return {
+                'html': code,
+            }
+class TangleKernel(TangleClient):
+    def tangle(self,html="""""", child=None,lang=None,output={}):
+        if lang and lang in self.env.globals['callback']:
+            html=self.render(child.outerHtml())
+            output = self.env.globals['callback'][lang](self.render(child.text()))
+        else:
+            html=self.render(html)
+        return self.weave(**{**output,**super().tangle(html)})
+class Tangle( TangleKernel):
+    templates=[]
+    def __init__(self, raw):
+        self.raw, self.frontmatter = [raw, {}]
+        if raw.startswith('---\n'):
+            tmp, self.raw = self.raw.lstrip('---').strip().split('---',1)
+            self.frontmatter=yaml.load(self.render(tmp))
+        super().__init__()
+    @property
+    def query(self):
+        return PyQuery(self.renderer(self.raw))
+    def _get_lang(self,child):
         lang = self.env.globals['default_lang']
         if child('code').attr('class'):
             lang = [c.lstrip(self.env.globals['lang_prefix']) for c in child('code').attr('class').split() if c.startswith(self.env.globals['lang_prefix'])][0]
-        filter_name = self.env.globals['filter_prefix']+lang
-        rendered = self.render(child.outerHtml(),data,render_data=True)
-        if filter_name in self.env.filters:
-            src = self.env.filters[filter_name](PyQuery(rendered).text())
-            rendered += """<script>%s</script>"""%src if src else """"""
-        return rendered
-
-class Templates(Compiler):
-    templates=[]
-    @property
-    def _current_template(self):
-        return self.env.globals[self.env.globals['_current_name']]
-
-    def render(self,txt, data={},render_data=False):
-        template = self.env.from_string(txt)
-        if '_current_name' in self.env.globals:
-            # Save the templates if teh cell is named
-            self._current_template.templates.append(template)
-        if self.env.globals['render_data'] or render_data:
-            return template.render({
-                **{k:getattr(self.env.ip.user_ns['__builtin__'],k) for k in dir(self.env.ip.user_ns['__builtin__']) if not k.startswith('_')},
-                **self.env.ip.user_ns, **data,
-            })
-        return txt
-
-class Selection(IPython.display.HTML, Templates):
-    def __init__(self,data):
-        super().__init__( data )
-        self.raw, self.query = [self.data, PyQuery(self.renderer(self.data))]
-        self.data = self.parse(self.query)
-    def parse( self, query=None, data={} ):
-        query = self.query if not query else query
+        return lang
+    def tangle(self):
         html, block, self.templates = ["""""","""""",[]]
-        for i, child in enumerate(query.children().items()):
+        for child in self.query.children().items():
             is_code = bool(child[0].tag in ['pre'])
             if not is_code:
                 block += child.outerHtml()
             if is_code and block:
-                html += '\n' + self.render(block, data)
+                html += '\n' + super().tangle(block)
                 block = """"""
             if is_code:
-                block += """<hr>%s<hr>"""%self.execute(child,data)
+                html+=super().tangle(child.text(),child=child,lang=self._get_lang(child))
         else:
-            html += '\n' + self.render(block, data) if block else """"""
+            html += '\n' + super().tangle(block) if block else """"""
         return html
-
-class Cell( Selection ):
-    renderer = mistune.Markdown( renderer=mistune.Renderer(escape=False))
-    def __init__(self, data, name=haikunator.haikunate(), env=LiterateEnvironment()):
-        self.env = env
-        self.name = name
+    def render(self,txt, data={},render_data=False):
+        template = self.env.from_string(txt)
+        self.templates.append(template)
+        if self.env.globals['render_data'] or render_data:
+            return template.render({
+                **{k:getattr(self.env.ip.user_ns['__builtin__'],k) for k in dir(self.env.ip.user_ns['__builtin__']) if not k.startswith('_')},
+                **self.env.ip.user_ns, **data, **self.frontmatter,
+            })
+        return txt
+class Cell(Tangle):
+    renderer=mistune.Markdown(renderer=mistune.Renderer(escape=False))
+    def __init__(self, raw, name="""_current_cell""", env=LiterateEnvironment()):
+        self.env, self.name = [env, name]
         self.env.globals[self.name] = self
-        super().__init__(data)
+        super().__init__(raw)
+
+class StaticCell(Cell, IPython.display.HTML):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.data=self.tangle()
+    def display(self):
+        return self
+
+def widgetize( cell, auto=False ):
+    if not hasattr(cell,'widgets'):
+        cell.widgets={}
+    if not 'html' in cell.widgets:
+        cell.widgets['html']=ipywidgets.HTML(cell.data)
+    def update_html(*args,**kwargs):
+        cell.widgets['html'].value = cell.tangle()
+    if not 'trigger' in cell.widgets:
+        cell.widgets['trigger']=ipywidgets.Button(description="""Update cell""")
+        cell.widgets['trigger'].on_click(callback=update_html)
+    def update_frontmatter(change,*args,**kwargs):
+        cell.frontmatter[change['owner'].description]=change['new']
+        if auto:
+            update_html()
+    for k, v in cell.frontmatter.items():
+        cell.widgets[k]=ipywidgets.interaction._widget_from_abbrev(v)
+        cell.widgets[k].description=k
+        cell.widgets[k].observe(names='value', handler=update_frontmatter)        
+    trigger = [] if auto else [cell.widgets['trigger']]
+    return ipywidgets.Box(children=[*[v for k,v in cell.widgets.items() if k in cell.frontmatter],*trigger,cell.widgets['html']])
 
 @IPython.core.magic.magics_class
 class Literate(IPython.core.magic.Magics):
-    def flush( self ):
-        [self.env.globals.pop(k,None) for k in self.cells.keys()]
-        self.cells = {}
-
-    def __init__(self, **kwargs):
+    def __init__(self, namespace='library', **kwargs):
         self.env, self.cells = [LiterateEnvironment(**kwargs), {}]
+        self.env.ip.user_ns[namespace] = self
         super().__init__()
         self.env.ip.register_magics(self)
-
     @IPython.core.magic.cell_magic
     @IPython.core.magic_arguments.magic_arguments()
     @IPython.core.magic_arguments.argument( "name", default=None, nargs="?", help="""Name of local variable to set to parsed value""")
     @IPython.core.magic_arguments.argument("-n", "--nodisplay", default=False, action="store_true", help="""Show the output""")
+    @IPython.core.magic_arguments.argument("-i", "--interact", default=False, action="store_true", help="""Show the output""")
+    @IPython.core.magic_arguments.argument("-a", "--auto", default=False, action="store_true", help="""Show the output""")
     def literate(self, line, cell):
         line = line.strip()
         args = IPython.core.magic_arguments.parse_argstring(self.literate, line)
-        define_global = True
+        cell = StaticCell( cell, args.name, self.env )
         if args.name:
-            self.env.globals['_current_name'] = args.name
-        else:
-            define_global = False
-            args.name = haikunator.haikunate(delimiter='_',tokenlength=0)
-        display = Cell( cell, args.name, self.env )
-        if define_global:
-            self.env.ip.user_ns[args.name] = display
+            self.env.ip.user_ns[args.name] = cell
         if not args.nodisplay:
-            return display
+            if args.interact:
+                return widgetize(cell,auto=args.auto)
+            return cell
