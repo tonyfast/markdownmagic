59e90990c577fedc73a3599f99f7132359b63b79
tonyfast <tony.fast@gmail.com>
2016-04-24 08:42:15 -0400

    Why wont it install

--- weave.py
+++
@@ -1,9 +0,0 @@
-class Weave(object):
-    def weave(self,**kwargs):
-        tokens={
-            'html':'',
-            'js':'',
-            'css':''
-        }
-        tokens.update(kwargs)
-        return self.env.get_template('weave_template').render(tokens)
